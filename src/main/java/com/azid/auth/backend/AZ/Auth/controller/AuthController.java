b46a2566e2ee82f4f8ac3420bda77a581fadfb81
abcd <awaisasad815@gmail.com>
2025-04-28 11:46:03 +0800

    User details in login response added

--- src/main/java/com/azid/auth/backend/AZ/Auth/controller/AuthController.java
+++ src/main/java/com/azid/auth/backend/AZ/Auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.azid.auth.backend.AZ.Auth.controller;
 import com.azid.auth.backend.AZ.Auth.model.AuthRequest;
 import com.azid.auth.backend.AZ.Auth.model.AuthResponse;
 import com.azid.auth.backend.AZ.Auth.model.User;
+import com.azid.auth.backend.AZ.Auth.repository.UserRepository;
 import com.azid.auth.backend.AZ.Auth.service.UserService;
 import com.azid.auth.backend.AZ.Auth.utils.JwtUtils;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,11 +24,13 @@ public class AuthController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
+    private final UserRepository userRepository;
 
-    public AuthController(UserService userService, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
+    public AuthController(UserService userService, AuthenticationManager authenticationManager, JwtUtils jwtUtils, UserRepository userRepository) {
         this.userService = userService;
         this.authenticationManager = authenticationManager;
         this.jwtUtils = jwtUtils;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/signup")
@@ -42,7 +46,9 @@ public class AuthController {
 
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         String jwtToken = jwtUtils.generateJwtToken(userDetails);
+        User user = userRepository.findByEmail(authRequest.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + authRequest.getEmail()));
 
-        return ResponseEntity.ok(new AuthResponse(jwtToken));
+        return ResponseEntity.ok(new AuthResponse(jwtToken, user.getEmail(), user.getPassword()));
     }
 }
